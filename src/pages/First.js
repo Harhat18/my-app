cddc8ec32fc10d03013d27c588ccc6cbab0baba2
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-11-15 23:51:08 +0300

    add

---
+++ src/pages/First.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function First() {
+  return (
+    <View>
+      <Text>Hello First</Text>
+    </View>
+  );
+}
---
+++ src/pages/Second.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Second() {
+  return (
+    <View>
+      <Text>Hello Second</Text>
+    </View>
+  );
+}
+
+export default Second;
